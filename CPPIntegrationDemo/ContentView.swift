1c78454b66136d2a150acc6501e9740d41ed8be3
Lexter Labra <lexterl@captioncall.onmicrosoft.com>
2020-01-08 10:25:32 +0800

    CID-1: Changed the signing identity

--- CPPIntegrationDemo/ContentView.swift
+++ CPPIntegrationDemo/ContentView.swift
@@ -1,21 +1,21 @@
+////
+////  ContentView.swift
+////  CPPIntegrationDemo
+////
+////  Created by Lexter Labra on 1/4/20.
+////  Copyright © 2020 Lexter Labra. All rights reserved.
+////
 //
-//  ContentView.swift
-//  CPPIntegrationDemo
+//import SwiftUI
 //
-//  Created by Lexter Labra on 1/4/20.
-//  Copyright © 2020 Lexter Labra. All rights reserved.
+//struct ContentView: View {
+//    var body: some View {
+//        Text("Hello, World!")
+//    }
+//}
 //
-
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
